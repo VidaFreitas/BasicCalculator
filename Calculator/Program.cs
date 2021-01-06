69c29628b68c679f500112aceaa51eaef604854a
Vida Valessa <vidavalessa65@gmail.com>
2021-01-06 15:53:29 -0300

    Calculator Subtraction

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,6 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Subtracao();
+        }
+
+        static void Soma()
+        {
             Console.Clear();
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
@@ -18,7 +23,26 @@ namespace Calculator
             float resultado = v1 + v2;
             Console.WriteLine("O resultado da soma é " + resultado);
             Console.WriteLine($"O resultado da soma é {resultado}");
-            Console.WriteLine("O resultado da soma é {v1+v2}");
+            // Console.WriteLine("O resultado da soma é {v1+v2}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtracao é {resultado}");
+            Console.ReadKey();
+
         }
     }
 }
