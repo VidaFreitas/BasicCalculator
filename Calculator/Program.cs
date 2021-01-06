99d2a241458d4bd00df099c61093b1fe7f3f5a26
Vida Valessa <vidavalessa65@gmail.com>
2021-01-06 16:16:04 -0300

    Calculator Multiplication

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Divisao();
+            Multiplicacao();
         }
 
         static void Soma()
@@ -58,6 +58,23 @@ namespace Calculator
 
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisao é {resultado}");
+            Console.ReadKey();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine("O resultado é" + (v1 * v2));
+
             Console.ReadKey();
         }
     }
