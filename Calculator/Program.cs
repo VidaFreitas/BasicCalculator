8cb6cfe593abf87cf33098edbae325aa2dc2dd49
Vida Valessa <vidavalessa65@gmail.com>
2021-01-06 16:46:35 -0300

    Calculator 2.0

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,10 +18,23 @@ namespace Calculator
             Console.WriteLine("2 - Subtracao");
             Console.WriteLine("3 - Divisao");
             Console.WriteLine("4 - Multiplicaçao");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("-----------");
             Console.WriteLine("Selecione uma opção:");
+
             short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: menu(); break;
+            }
+
         }
 
         static void Soma()
@@ -40,6 +53,7 @@ namespace Calculator
             Console.WriteLine($"O resultado da soma é {resultado}");
             // Console.WriteLine("O resultado da soma é {v1+v2}");
             Console.ReadKey();
+            menu();
         }
 
         static void Subtracao()
@@ -57,6 +71,7 @@ namespace Calculator
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtracao é {resultado}");
             Console.ReadKey();
+            menu();
 
         }
         static void Divisao()
@@ -74,6 +89,7 @@ namespace Calculator
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisao é {resultado}");
             Console.ReadKey();
+            menu();
         }
         static void Multiplicacao()
         {
@@ -91,6 +107,7 @@ namespace Calculator
             Console.WriteLine("O resultado é" + (v1 * v2));
 
             Console.ReadKey();
+            menu();
         }
     }
 }
