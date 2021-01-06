3e75f5ccd0cdb5e17e1e1a4b62f679354a28b2a8
Vida Valessa <vidavalessa65@gmail.com>
2021-01-06 16:04:58 -0300

    Calculator Division

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Subtracao();
+            Divisao();
         }
 
         static void Soma()
@@ -43,6 +43,22 @@ namespace Calculator
             Console.WriteLine($"O resultado da subtracao é {resultado}");
             Console.ReadKey();
 
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor; ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisao é {resultado}");
+            Console.ReadKey();
         }
     }
 }
